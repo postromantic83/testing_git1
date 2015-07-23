b5b451cdf97b3c0ae638efaf2db8306463aea109
postromantic83 <Agile.brain83@gmail.com>
2015-07-23 16:23:00 +0300

    Initial commit

---
+++ src/main/java/pack1/Main.java
@@ -0,0 +1,17 @@
+package pack1;
+
+/**
+ * Created by ShurikNote on 23.07.2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    System.out.print("hello world!");
+
+
+    }
+
+
+
+}

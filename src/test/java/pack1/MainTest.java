85ba16af526f1c0bf2b3188e118cbf9a6ef0ecd7
postromantic83 <Agile.brain83@gmail.com>
2015-07-27 12:51:09 +0300

    second commit

---
+++ src/test/java/pack1/MainTest.java
@@ -0,0 +1,24 @@
+package pack1;
+
+import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by ShurikNote on 27.07.2015.
+ */
+public class MainTest {
+
+    @InjectMocks
+    private Main main;
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+
+
+
+
+    }
+}

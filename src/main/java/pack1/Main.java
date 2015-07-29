bb353bd343db4f44331e203c6437e279674b5868
postromantic83 <Agile.brain83@gmail.com>
2015-07-29 12:59:58 +0300

    second commit

--- src/main/java/pack1/Main.java
+++ src/main/java/pack1/Main.java
@@ -7,17 +7,44 @@ import org.slf4j.LoggerFactory;
  * Created by ShurikNote on 23.07.2015.
  */
 public class Main {
-
+    public double doubleA;
+    public double doubleB;
+    public double doubleResult;
+    public float floatA,floatB,floatResult;
     public static void main(String[] args) {
 
         Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.debug("Hello World!!!");
 
-    //System.out.print("hello world!");
+        Main m = new Main();
+        m.setParams(0.3,0.4,0.3f,0.4f);
+        m.doublesumm(m.doubleA, m.doubleB);
+        m.floatsumm(m.floatA, m.floatB);
 
+
+        logger.info("Float result  a + b = " + m.floatResult);
+        logger.info("Double result  a + b = " + m.doubleResult);
+
+
+    }
+
+    //intresting sample
+    public void setParams (double doubleA, double doubleB, float floatA,float floatB ){
+        this.doubleA = doubleA;
+        this.doubleB = doubleB;
+        this.floatA = floatA;
+        this.floatB = floatB;
+    }
+
+    public void doublesumm(double a,double b) {
+        this.doubleResult = a+b;
+    }
+
+    public void floatsumm(float a,float b) {
+        this.floatResult = a+b;
+    }
 
     }
 
 
 
-}
+
--- src/test/java/pack1/MainTest.java
+++ src/test/java/pack1/MainTest.java
@@ -1,24 +1,44 @@
 package pack1;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
 import static org.junit.Assert.*;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import static org.junit.Assert.assertEquals;
+
+
 
 /**
  * Created by ShurikNote on 27.07.2015.
  */
 public class MainTest {
 
-    @InjectMocks
-    private Main main;
+    public static final float DELTA = 0.00000005f;
+    public static final double DELTA2 = 0.0;
+        private  double doubleA;
+        private  double doubleB;
+        private  double doubleResult;
+        private  float floatA, floatB, floatResult;
+        Main m;
 
-    @org.junit.Test
-    public void testMain() throws Exception {
+    @Before
+    public void setUp(){
+        m = new Main();
+        m.setParams(0.8,0.2,0.8f,0.2f);
+        }
 
+    @Test
+    public void returnsInvalidFloatResult () {
 
-
-
-
+       m.floatsumm(m.floatA, m.floatB);
+       assertEquals("Ничоси!",1.0f,m.floatResult , DELTA);
     }
+    @Test
+    public void returnsInvalidDoubleResult() {
+
+
+        m.doublesumm(m.doubleA, m.doubleB);
+        assertEquals("Ничоси!",1.0,m.doubleResult , DELTA2);
+    }
+
 }
